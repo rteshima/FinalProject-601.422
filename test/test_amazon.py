de41e284072fb043d3f6cbb0f3ba8c0eed6e758b
ryanteshima <rteshim1@jhu.edu>
2021-04-21 15:16:03 -0400

    setup test folder environment for us to use for unit tests

---
+++ test/test_amazon.py
@@ -0,0 +1,11 @@
+import stores.amazon as amazon
+import unittest
+
+class AmazonTestCase(unittest.TestCase):
+    # setUp function where we'll initialize objects
+    def setUp(self):
+        pass
+
+    # again, use this format
+    def test_amazon_function_number_1(self):
+        pass
