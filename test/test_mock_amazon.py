dce510f6dd3b75ce7241465e20dd8f057f7f181b
Ravina Sashti <ravina0101@gmail.com>
2021-04-29 10:05:29 -0400

    adding navigate_pages case tests

--- test/test_mock_amazon.py
+++ test/test_mock_amazon.py
@@ -14,42 +14,117 @@ class MockAmazonTestCase(unittest.TestCase):
     def tearDown(self):
         self.am = None
 
-    def test_items_already_in_cart(self):
-        print("testing run")
-        with self.assertLogs(logger='fairgame', level='INFO') as self.cm:
-            #self.am.asin_list = [["B08FC5L3RG"]]
-            self.am.run(delay=1)
-            self.am.run(delay=1)
 
-        
-        self.assertIn(
-            "INFO:fairgame:Delete all item(s) in cart before starting bot.",
-            self.cm.output
-        )
-
-    def test_fail_to_checkout_note(self):
-        print("testing fail to checkout note")
-        with self.assertLogs(logger='fairgame', level='INFO') as self.cm:
-            #self.am.asin_list = [["B08FC5L3RG"]]
-            self.am.fail_to_checkout_note()
-
-        
-        self.assertIn(
-            "INFO:fairgame:It's likely that the product went out of stock before FairGame could checkout.",
-            self.cm.output
-        )
-
-
-    def test_navigate_pages(self):
-        print("testing navigate pages with test = true")
-        self.am.get_page(url="https://"+self.am.amazon_website)
+    def test_navigate_pages_signin(self):
+        print("[INITIALIZING TEST]: test_navigate_pages_signin")
         with self.assertLogs(logger='fairgame', level='DEBUG') as self.cm:
+            self.am.amazon_config = {"SIGN_IN_TITLES": ["Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more"]}
+            self.am.get_page(url="https://"+self.am.amazon_website)
             self.am.navigate_pages(True)
-        
         self.assertIn(
             "DEBUG:fairgame:'navigate_pages' returned None",
             self.cm.output
         )
+
+    def test_navigate_pages_shoppingcart(self):
+        print("[INITIALIZING TEST]: test_navigate_pages_shoppingcart")
+        with self.assertLogs(logger='fairgame', level='DEBUG') as self.cm:
+            self.am.amazon_config = {"SHOPPING_CART_TITLES": ["Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more"]}
+            self.am.get_page(url="https://"+self.am.amazon_website)
+            self.am.navigate_pages(True)
+        self.assertIn(
+            "DEBUG:fairgame:'navigate_pages' returned None",
+            self.cm.output
+        )
+
+    def test_navigate_pages_checkout(self):
+        print("[INITIALIZING TEST]: test_navigate_pages_checkout")
+        with self.assertLogs(logger='fairgame', level='DEBUG') as self.cm:
+            self.am.amazon_config = {"CHECKOUT_TITLES": ["Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more"]}
+            self.am.get_page(url="https://"+self.am.amazon_website)
+            self.am.navigate_pages(True)
+        self.assertIn(
+            "DEBUG:fairgame:'navigate_pages' returned None",
+            self.cm.output
+        )
+
+    def test_navigate_pages_ordercomplete(self):
+        print("[INITIALIZING TEST]: test_navigate_pages_ordercomplete")
+        with self.assertLogs(logger='fairgame', level='DEBUG') as self.cm:
+            self.am.amazon_config = {"ORDER_COMPLETE_TITLES": ["Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more"]}
+            self.am.get_page(url="https://"+self.am.amazon_website)
+            self.am.navigate_pages(True)
+        self.assertIn(
+            "DEBUG:fairgame:'navigate_pages' returned None",
+            self.cm.output
+        )
+
+    def test_navigate_pages_prime_titles(self):
+        print("[INITIALIZING TEST]: test_navigate_pages_prime_titles")
+        with self.assertLogs(logger='fairgame', level='DEBUG') as self.cm:
+            self.am.amazon_config = {"PRIME_TITLES": ["Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more"]}
+            self.am.get_page(url="https://"+self.am.amazon_website)
+            self.am.navigate_pages(True)
+        self.assertIn(
+            "DEBUG:fairgame:'navigate_pages' returned None",
+            self.cm.output
+        )
+
+    def test_navigate_pages_homepage(self):
+        print("[INITIALIZING TEST]: test_navigate_pages_homepage")
+        with self.assertLogs(logger='fairgame', level='DEBUG') as self.cm:
+            self.am.amazon_config = {"HOME_PAGE_TITLES": ["Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more"]}
+            self.am.get_page(url="https://"+self.am.amazon_website)
+            self.am.navigate_pages(True)
+        self.assertIn(
+            "DEBUG:fairgame:'navigate_pages' returned None",
+            self.cm.output
+        )
+
+    def test_navigate_pages_doggo(self):
+        print("[INITIALIZING TEST]: test_navigate_pages_doggo")
+        with self.assertLogs(logger='fairgame', level='DEBUG') as self.cm:
+            self.am.amazon_config = {"DOGGO_TITLES": ["Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more"]}
+            self.am.get_page(url="https://"+self.am.amazon_website)
+            self.am.navigate_pages(True)
+        self.assertIn(
+            "DEBUG:fairgame:'navigate_pages' returned None",
+            self.cm.output
+        )
+
+    def test_navigate_pages_outofstock(self):
+        print("[INITIALIZING TEST]: test_navigate_pages_outofstock")
+        with self.assertLogs(logger='fairgame', level='DEBUG') as self.cm:
+            self.am.amazon_config = {"OUT_OF_STOCK": ["Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more"]}
+            self.am.get_page(url="https://"+self.am.amazon_website)
+            self.am.navigate_pages(True)
+        self.assertIn(
+            "DEBUG:fairgame:'navigate_pages' returned None",
+            self.cm.output
+        )
+
+    def test_navigate_pages_business_po(self):
+        print("[INITIALIZING TEST]: test_navigate_pages_business_po")
+        with self.assertLogs(logger='fairgame', level='DEBUG') as self.cm:
+            self.am.amazon_config = {"BUSINESS_PO_TITLES": ["Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more"]}
+            self.am.get_page(url="https://"+self.am.amazon_website)
+            self.am.navigate_pages(True)
+        self.assertIn(
+            "DEBUG:fairgame:'navigate_pages' returned None",
+            self.cm.output
+        )
+
+    def test_navigate_pages_address_select(self):
+        print("[INITIALIZING TEST]: test_navigate_pages_address_select")
+        with self.assertLogs(logger='fairgame', level='DEBUG') as self.cm:
+            self.am.amazon_config = {"ADDRESS_SELECT": ["Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more"]}
+            self.am.get_page(url="https://"+self.am.amazon_website)
+            self.am.navigate_pages(True)
+        self.assertIn(
+            "DEBUG:fairgame:'navigate_pages' returned None",
+            self.cm.output
+        )
+
 
 
 if __name__ == '__main__':
